02429d9f03a69afe3cdf070f07edb7ffb9cd2e0b
gaoyabin <gaoyabing@126.com>
2018-07-06 18:07:02 +0800

    1
    1

---
+++ Get History Msg Links for WeChat.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         获取微信公众号历史消息链接
+// @version      0.1
+// @description  Get History Msg Links for WeChat
+// @namespace    CharyGao.Tampermonkey.user.js
+// @author       CharyGao
+// @match        http://mp.weixin.qq.com/s?*
+// @match        https://mp.weixin.qq.com/s?*
+// @match        https://mp.weixin.qq.com/s?*
+// @match        http://mp.weixin.qq.com/s/*
+// @match        file:///*
+// @grant        GM_addStyle
+// @run-at       document-start
+// @require     https://cdn.bootcss.com/jquery/3.3.1/jquery.js
+// ==/UserScript==
+
+GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} #down_url_links{white-space:nowrap!important;background-color: orange!important;}' );
+(function ($) {
+    'use strict';
+    $(function () {
+        if ($('#down_url')) {
+            $('#down_url').remove();
+        };
+        $(document.body).prepend('<div id="down_url" ></div>');
+        $('#down_url').append('links count: , ' + $('h4.weui_media_title').length + '<br>');
+
+        $('h4.weui_media_title').each(function (i) {
+            var link_href = $(this).attr('hrefs');
+
+            if (link_href) {
+                var down_link = '<div id="down_url_links">'+i + ' , ' + link_href +  ' , ' +$(this).text() + '</div>';
+                $('#down_url').append(down_link);
+            }
+        }
+
+        );
+        //$('#down_url').append('</table>');
+    });
+})(jQuery.noConflict(true));
+
+
