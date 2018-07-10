9d983c3507925cf9c03c02f493feb2fff7aa9ffd
gaoyabin <gaoyabing@126.com>
2018-07-10 17:10:29 +0800

    6
    6

--- Get History Msg Links for WeChat.js
+++ Get History Msg Links for WeChat.js
@@ -15,13 +15,25 @@
 // @icon         https://res.wx.qq.com/zh_CN/htmledition/v2/images/favicon31e225.ico
 // ==/UserScript==
 
-GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} '+
-            '#down_url_links{white-space:nowrap!important;background-color: orange!important;} '+
-            '#img-content{position: fixed !important;z-index: 9999970 !important;top: 20px !important;left: 2% !important;height: 100% !important;width: 96% !important;overflow: scroll !important;}'+
-            '#js_pc_qr_code{visibility: hidden;}');
+GM_addStyle('span.weui_media_hd{width:100px!important;float:left!important;} ' +
+    '#down_url_links{white-space:nowrap!important;background-color:orange!important;} ' +
+    '#img-content{position:fixed!important;z-index:100!important;top:20px!important;left:2%!important;height:100%!important;width:96%!important;overflow:scroll!important;}' +
+    '#js_pc_qr_code{visibility:hidden!important;}' +
+    '#page-content{background-color:rgba(0,0,0,0)!important;}' +
+    '#js_profile_qrcode_img{height:100px!important;width:100px!important;}' +
+    'img{width:100%!important;height:auto!important;}');//直接下载所有图片
 (function ($) {
     'use strict';
     $(function () {
+        $('img').each(function () {//直接下载所有图片
+            var dataSrc = $(this).attr('data-src');
+            if (dataSrc) {
+                $(this).attr('src', dataSrc);
+                $(this).attr('_src', dataSrc);
+                $(this).removeAttr('data-src');
+
+            }
+        });
         if ($('#down_url')) {
             $('#down_url').remove();
         };
@@ -41,6 +53,5 @@ GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} '
         }
 
         );
-        //$('#down_url').append('</table>');
     });
 })(window.jQuery.noConflict(true));
