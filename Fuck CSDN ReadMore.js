a643dc9fe80e3c2bcf86b2fba99a39d2d64cc18c
gaoyabin <gaoyabing@126.com>
2018-07-10 09:08:16 +0800

    2
    2

---
+++ Fuck CSDN ReadMore.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         CSDN 全文阅读你大爷
+// @namespace    http://www.cnblogs.com/Chary/
+// @version      0.1
+// @description  把阅读更多的那个啥玩意儿给干掉
+// @author       CharyGao
+// @match        http://blog.csdn.net/*/article/details/*
+// @match        https://blog.csdn.net/*/article/details/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var ef = document.querySelector('.hide-article-box');
+    if (ef) {
+        ef.remove();
+        document.querySelector('#article_content').style.height='auto';
+    }
+
+    var btnMore = document.getElementById("btn-readmore");
+    if (btnMore != undefined) {
+        btnMore.click();
+    } else {
+        console.log("No button found.");
+    }
+})();
--- Get History Msg Links for WeChat.js
+++ Get History Msg Links for WeChat.js
@@ -2,7 +2,7 @@
 // @name         获取微信公众号历史消息链接
 // @version      0.1
 // @description  Get History Msg Links for WeChat
-// @namespace    CharyGao.Tampermonkey.user.js
+// @namespace    http://www.cnblogs.com/Chary/
 // @author       CharyGao
 // @match        http://mp.weixin.qq.com/s?*
 // @match        https://mp.weixin.qq.com/s?*
@@ -12,9 +12,10 @@
 // @grant        GM_addStyle
 // @run-at       document-start
 // @require     https://cdn.bootcss.com/jquery/3.3.1/jquery.js
+// @icon         https://res.wx.qq.com/zh_CN/htmledition/v2/images/favicon31e225.ico
 // ==/UserScript==
 
-GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} #down_url_links{white-space:nowrap!important;background-color: orange!important;}' );
+GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} #down_url_links{white-space:nowrap!important;background-color: orange!important;}');
 (function ($) {
     'use strict';
     $(function () {
@@ -24,11 +25,14 @@ GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} #
         $(document.body).prepend('<div id="down_url" ></div>');
         $('#down_url').append('links count: , ' + $('h4.weui_media_title').length + '<br>');
 
+        $('#js_view_source').href = window.msg_source_url;
+        $('#js_view_source').target = '_blank';
+
         $('h4.weui_media_title').each(function (i) {
             var link_href = $(this).attr('hrefs');
 
             if (link_href) {
-                var down_link = '<div id="down_url_links">'+i + ' , ' + link_href +  ' , ' +$(this).text() + '</div>';
+                var down_link = '<div id="down_url_links">' + i + ' , ' + link_href + ' , ' + $(this).text() + '</div>';
                 $('#down_url').append(down_link);
             }
         }
@@ -36,6 +40,7 @@ GM_addStyle('span.weui_media_hd{width:100px !important;float:left !important;} #
         );
         //$('#down_url').append('</table>');
     });
-})(jQuery.noConflict(true));
+})(window.jQuery.noConflict(true));
+
 
 
