09a9fb8e87ff482b4e02b7d2d85cd7d833c0afd0
gaoyabin <gaoyabing@126.com>
2018-07-30 16:03:47 +0800

    3
    3

--- Fuck Hexun SeeAll.js
+++ Fuck Hexun SeeAll.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         和讯理财看全文你大爷
 // @namespace    http://www.cnblogs.com/Chary/
-// @version      0.1
+// @version      0.2
 // @description  把看全文的那个啥玩意儿给干掉
 // @author       CharyGao
 // @match        http://money.hexun.com/*
@@ -9,7 +9,10 @@
 // @icon         http://www.hexun.com/favicon.ico
 
 // ==/UserScript==
-GM_addStyle('.art_contextBox{height:auto!important;} .fttBox{display: none!important;}' 
+GM_addStyle(
+	'.art_contextBox{height:auto!important;}'
+	+' .appDl{display: none!important;}'
+	+' .fttBox{display: none!important;}' 
 );//直接下载所有图片
 (function() {
     'use strict';
